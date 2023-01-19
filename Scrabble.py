833a740b3e48b27a2e7a64bb8ff04aae2f2b9303
Sam-Berri <107113152+Sam-Berri@users.noreply.github.com>
2023-01-19 13:47:29 +0000

    Create Scrabble.py

---
+++ Scrabble.py
@@ -0,0 +1,45 @@
+letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+points = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 4, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10]
+
+letters += [ letter.lower() for letter in letters]
+points *= 2
+
+letters_to_points = {letters:points for letters, points in zip(letters, points)}
+
+
+print(letters_to_points)
+
+letters_to_points[" "] = 0
+
+def score_word(word):
+  point_total = 0
+  for letters in word:
+    point_total += letters_to_points.get(letters, 0)
+  return point_total
+
+brownie_points = score_word("BROWNIE")
+print(brownie_points)
+
+
+players_to_words = {"player1":["BLUE", "TENNIS", "EXIT"] , "wordNerd":["EARTH", "EYES", "MACHINE"], "lexi Con" : ["ERASER" , "BELLY", "HUSKY"], "Prof Reader":["ZAP", "COMA", "PERIOD"]  } 
+
+player_to_points = {}
+
+def update_point_totals():
+  for player, words in players_to_words.items():
+    player_points = 0
+    for word in words:
+      player_points += score_word(word)
+    player_to_points[player] = player_points
+
+update_point_totals()
+print(player_to_points)
+
+
+def play_word(player,word):
+  players_to_words[player].append(word)
+
+play_word("player1", "CODE")
+print(players_to_words)
+
+
